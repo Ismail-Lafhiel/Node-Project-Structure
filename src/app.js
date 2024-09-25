4bbeb068b2eef4da98dc04bea0de71eb6afbced3
Ismail-Lafhiel <odashimidorida@gmail.com>
2024-09-25 15:07:58 +0100

    final commit

--- src/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userRoutes = require('./api/v1/routes/userRoutes');
-const app = express();
-
-app.use(express.json()); // To parse JSON bodies
-app.use('/api/v1/users', userRoutes); // Use the user routes
-
-app.listen(3001, () => console.log('Server running on port 3001'));
